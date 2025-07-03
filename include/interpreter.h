9f019a9678f4454618ee6926fd1fd2b673518579
AlekSimpson <alek@asu.me>
2025-07-02 18:14:28 -0700

    stashing

--- include/interpreter.h
+++ include/interpreter.h
@@ -7,27 +7,22 @@
 
 #define LOGIC_REG 21
 #define SYSCALL_REG 22
-#define PROGRAM_POINTER 23
-#define STACK_POINTER 24
+#define STACK_POINTER 23
 
 #define offset_t int
 
 class StackFrame {
 public:
-  int stack_height;
-  stack<string> current_scope_stack;
-  unordered_map<string, offset_t> allocations; // tracks variable allocations
+  int return_address; 
+  int stack_pointer;
+  StackFrame* previous_frame;
+  map<int, int> saved_caller_values;
+  map<string, offset_t> local_variables; // tracks variable allocations
 
-  // tracks where the scopes of certain functions begin
-  // used to know how much to collapse the stack by when a function returns
-  unordered_map<string, int> function_scopes;
-
-  StackFrame();
-  
-  void add_allocation(string name); // increments the stack height
-  offset_t get_allocation_offset(string name);
-  void push_stack_scope(string scopename);
-  void pop_stack_scope();
+  StackFrame(StackFrame* previous, 
+	     map<int, int> caller_cache, 
+	     int ret_addr, 
+	     int stack_pointer);
 };
 
 class SageInterpreter {
@@ -36,19 +31,19 @@ public:
   // - 0-5  = function parameter registers
   // - 6-9  = return value registers
   // - 10-20 = general registers
-  // - 21-24 = system registers
-  // - 25-30 = volatile registers (hold results of temp values and stuff)
+  // - 21-23 = system registers
+  // - 24-30 = volatile registers (hold results of temp values and stuff)
   //
   // sr21 = bool logical result register
-  // sr22 = syscall register
-  // sr23 = subroutine return register, holds program pointer
-  // sr24 = stack pointer
+  // sr22 = subroutine return register, holds program pointer
+  // sr23 = stack pointer
   //
 
-  StackFrame stack_frame;
   int registers[31];
+  int program_pointer;
   int procedure_encoding; // starts at 0
   map<string, int> procedure_label_encoding;
+  StackFrame* frame_pointer; // keeps track of current frame
 
   bytecode program;
   map<int, SageValue> heap;
@@ -57,9 +52,15 @@ public:
   SageInterpreter();
   SageInterpreter(int _stack_size);
 
+  void close();
+
   int store_in_heap(SageValue value);
   void load_program(bytecode program);
   void execute(int begin_program_counter);
+
+  void push_stack_scope(string scopename);
+  void pop_stack_scope(); // pops current stack frame
+  vector<int> dereference_map(instruction*, int[4]);
 };
 
 
--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -5,45 +5,41 @@
 
 // StackFrame
 
-StackFrame::StackFrame() {}
-
-void StackFrame::add_allocation(string name) {
-    stack_height++;
-    string current_scope = current_scope_stack.top();
-    allocations[name] = stack_height - current_scope;
-}
-
-offset_t StackFrame::get_allocation_offset(string name) {
-    if (allocations[name] > stack_height) {
-        // ERROR!! ATTEMPTING TO REFERENCE FREED STACK VARIABLE 
-        return -1;
-    }
-
-    return allocations[name];
-}
-
-void StackFrame::push_stack_scope(string scopename) {
-    function_scopes[scopename] = stack_height + 1;
-    current_scope_stack.push(scopename);
-}
-
-void StackFrame::pop_stack_scope() {
-    string scopename = current_scope_stack.top();
-    current_scope_stack.pop();
-
-    int difference = stack_height - function_scopes[scopename];
-    stack_height = stack_height - difference;
-    function_scopes.erase(scopename);
-}
-
+StackFrame::StackFrame(
+    StackFrame* previous, map<int, int> caller_cache, int ret_addr, int sp
+) : return_address(ret_addr), stack_pointer(sp), previous_frame(previous), 
+    saved_caller_values(register_cache) {}
 
 // Interpreter
 
-SageInterpreter::SageInterpreter() {}
+SageInterpreter::SageInterpreter();
 
 SageInterpreter::SageInterpreter(int stack_size) 
     : stack_frame(StackFrame()) {
     stack.reserve(stack_size);
+}
+
+void SageInterpreter::push_stack_scope() {
+    if (registers[STACK_POINTER]+1 == stack.size()) {
+	// ERROR: STACKOVERFLOW !!!!
+        return;
+    }
+
+    // make new current stack frame
+    int return_address = registers[STACK_POINTER];
+    registers[STACK_POINTER]++;
+    map<int, int> cached_registers = frame_pointer->saved_caller_values;
+    frame_pointer = new StackFrame(frame_pointer, 
+		    	           cached_registers, 
+				   return_address,
+				   regsters[STACK_POINTER]);
+}
+
+void SageInterpreter::pop_stack_scope() {
+    registers[STACK_POINTER] = frame_pointer->stack_pointer-1;
+    StackFrame* previous = frame_pointer->previous_frame;
+    delete frame_pointer;
+    frame_pointer = previous;
 }
 
 void SageInterpreter::load_program(bytecode _program) {
@@ -223,10 +219,29 @@ void SageInterpreter::execute(int begin_program_counter) {
                 break;
         }
 
-        registers[PROGRAM_POINTER]++;
-        current_instruction = program[registers[PROGRAM_POINTER]].instruction;
-        inst_map = program[registers[PROGRAM_POINTER]].map;
+	program_pointer++;
+        current_instruction = program[program_pointer].instruction;
+        inst_map = program[program_pointer].map;
     }
 }
+
+int store_in_heap(SageValue value) {
+    int pointer = heap.size();
+    heap[pointer] = value;
+    return pointer;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
