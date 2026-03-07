26dadc46e9fed03528d3093ee4503c075bcce17d
AlekSimpson <alek@asu.me>
2026-03-07 13:56:05 -0700

    stashing

--- build/objects/src/node_manager.d
+++
@@ -1,4 +0,0 @@
-build/objects/src/node_manager.o: src/node_manager.cpp \
-  include/error_logger.h include/token.h src/../include/node_manager.h \
-  src/../include/parse_node.h src/../include/token.h \
-  src/../include/scope_manager.h
